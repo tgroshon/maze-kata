d588c860fb79a1357da0e13ac1ca34dc66bfa419
Tommy Groshong <tom@groshong.software>
2021-11-15 16:59:05 -0500

    Remove unused function from solver

--- maze_kata/lib/solver.py
+++ maze_kata/lib/solver.py
@@ -16,10 +16,3 @@ class DefaultStrategy:
             return [[1, idx + 1]]
 
         return None
-
-
-def get_row(data, row_num):
-    if row_num <= 0:
-        raise Exception(f"Rows are 1-based, got: {row_num}")
-
-    return data[row_num - 1]
