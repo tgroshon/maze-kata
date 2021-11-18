1090fed42bb283972595c75e4ee9a7df59279c82
Tommy Groshong <tom@groshong.software>
2021-11-17 23:31:10 -0500

    Refactor solver to use iterative, explicit stack DFS
    This had an unintended side-effect of SUPER EFFICIENTLY solving the maze because
    it accidentally priortized DOWNWARD movement.
    
    An artifact of the `Maze::get_adjacent_spaces` method is that it's always
    returning `[left, right, above, below]` ordering. The recursive approach stepped
    through directions from the beginning of the list, while the iterative approach
    using a stack stepped through directions from the end.
    
    The trick with these mazes is apparently to always move down when you can.

--- maze_kata/lib/solver.py
+++ maze_kata/lib/solver.py
@@ -8,34 +8,39 @@ def solve(maze):
 
 class DefaultStrategy:
     def solve(self, maze):
+        """Basically a naive depth-first search
+
+        This should always find the end space, but may not find the shortest path.
+
+        Complexities:
+          - Time: O(Vertices + Edges)
+          - Space: O(Vertices)
+
+        FIXME: Naively returning the 'visited' data structure doesn't work
+        great for making a 'path', because it overreports steps for mazes with
+        rooms or deadends. Refactor to return the pruned steps to end.
+
+        """
         start_space = maze.get_start_space()
 
         if maze.shape.rows == 1:
             return [start_space]
 
         visited = []
-        self.search(visited, maze, start_space)
+        stack = [start_space]
+
+        while len(stack):
+            address = stack.pop()
+
+            if address not in visited:
+                visited.append(address)
+
+            if maze.is_end(address):
+                """Break the loop we're done!"""
+                break
+
+            for neighbor in maze.get_adjacent_spaces(address):
+                if neighbor not in visited:
+                    stack.append(neighbor)
+
         return visited
-
-    def search(self, visited, maze, address):
-        """Basically a naive depth-first search
-
-        This will always find the end space. The 'stack' is implicitly the (a)
-        adjacent space iteration and (b) the python frame stack.
-
-        Complexities:
-          - Time: O(Vertices + Edges)
-          - Space: O(Vertices)
-
-        FIXME: Naively returning the 'visited' data structure doesn't work for
-        making a 'path', so it will give the wrong answer for mazes with rooms
-        or deadends. Refactor to return the pruned steps to get to the end.
-
-        """
-        visited.append(address)
-        if maze.is_end(address):
-            return
-
-        for neighbor in maze.get_adjacent_spaces(address):
-            if neighbor not in visited:
-                self.search(visited, maze, neighbor)
