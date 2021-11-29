75720320d0fe5314628b5238cbb65faeacce123d
Tommy Groshong <tom@groshong.software>
2021-11-29 13:21:51 -0500

    Add benchmark case for ImprovedStrategy

--- benchmark.py
+++ benchmark.py
@@ -3,7 +3,6 @@ import tempfile
 import os
 from maze_kata.lib import digitizer, core
 
-print("done")
 
 INPUT_IMG_PATH = os.path.join("assets", "us6_complex.png")
 
@@ -24,9 +23,23 @@ def get_callable_for_solver():
     return run_solver
 
 
+def get_callable_for_improved_strategy_solver():
+    maze = digitizer.parse_maze_image(INPUT_IMG_PATH)
+
+    def run_solver():
+        core.solve(maze, core.ImprovedStrategy())
+
+    return run_solver
+
+
 if __name__ == "__main__":
     result = timeit.timeit(get_callable_for_solver(), number=100)
     print("Solver with DefaultStrategy benchmark 100x:", result)
 
+    result = timeit.timeit(get_callable_for_improved_strategy_solver(), number=100)
+    print("Solver with ImprovedStrategy benchmark 100x:", result)
+
     result = timeit.timeit(run_full_pipeline, number=100)
     print("Full pipeline Load->Solve->Output 100x:", result)
+
+    print("Done.")
